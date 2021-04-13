7ef65c041d7b56c1823209450362ea6156d37317
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-04-13 10:51:36 +0300

    fixed some problem

--- js/app.js
+++ js/app.js
@@ -22,26 +22,26 @@ function Product(name, filePath) {
     Product.productsArray.push(this)
 }
 
-new Product('bag', `/img/bag.jpg`);
-new Product('banana', `/img/banana.jpg`);
-new Product('bathroom', `/img/bathroom.jpg`);
-new Product('boots', `/img/boots.jpg`);
-new Product('breakfast', `/img/breakfast.jpg`);
-new Product('bubblegum', `/img/bubblegum.jpg`);
-new Product('chair', `/img/chair.jpg`);
-new Product('cthulhu', `/img/cthulhu.jpg`);
-new Product('dog-duck', `/img/dog-duck.jpg`);
-new Product('dragon', `/img/dragon.jpg`);
-new Product('pen', `/img/pen.jpg`);
-new Product('pet-sweep', `/img/pet-sweep.jpg`);
-new Product('scissors', `/img/scissors.jpg`);
-new Product('shark', `/img/shark.jpg`);
-new Product('sweep', `/img/sweep.png`);
-new Product('tauntaun', `/img/tauntaun.jpg`);
-new Product('unicorn', `/img/unicorn.jpg`);
-new Product('usb', `/img/usb.gif`);
-new Product('water-can', `/img/water-can.jpg`);
-new Product('wine-glass', `/img/wine-glass.jpg`);
+new Product('bag', `img/bag.jpg`);
+new Product('banana', `img/banana.jpg`);
+new Product('bathroom', `img/bathroom.jpg`);
+new Product('boots', `img/boots.jpg`);
+new Product('breakfast', `img/breakfast.jpg`);
+new Product('bubblegum', `img/bubblegum.jpg`);
+new Product('chair', `img/chair.jpg`);
+new Product('cthulhu', `img/cthulhu.jpg`);
+new Product('dog-duck', `img/dog-duck.jpg`);
+new Product('dragon', `img/dragon.jpg`);
+new Product('pen', `img/pen.jpg`);
+new Product('pet-sweep', `img/pet-sweep.jpg`);
+new Product('scissors', `img/scissors.jpg`);
+new Product('shark', `img/shark.jpg`);
+new Product('sweep', `img/sweep.png`);
+new Product('tauntaun', `img/tauntaun.jpg`);
+new Product('unicorn', `img/unicorn.jpg`);
+new Product('usb', `img/usb.gif`);
+new Product('water-can', `img/water-can.jpg`);
+new Product('wine-glass', `img/wine-glass.jpg`);
 
 
 console.log(Product.productsArray);
@@ -160,6 +160,7 @@ function handleUsersClick(event) {
     } else {
 
         let ButtonElement = document.getElementById('resultsButton');
+        ButtonElement.hidden=false;
 
         ButtonElement.addEventListener('click', clickToView);
 
